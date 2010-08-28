8fb0ab2a0d66836fce7571c8e0c9c348dcd2e4f8
Toms Bauģis <toms.baugis@gmail.com>
2010-08-28 03:22:32 +0100

    updates for v2, stripped to the minimum i need right now

--- lib/etsy.py
+++ lib/etsy.py
@@ -1,5 +1,6 @@
 from google.appengine.api import memcache
 import datetime as dt
+import logging
 
 from urllib import urlopen, urlencode
 try:
@@ -8,191 +9,67 @@ except ImportError:
     import json
 
 
-class Etsy(object):
-    base_url = "http://beta-api.etsy.com/v1"
-
-    def __init__(self, api_key):
-        self.__api_key = api_key
-    
-    def _make_call(self, path, params = {}):
-        basedict = {"api_key":self.__api_key}
-        basedict.update(params)
-        fullurl = self.base_url + path + '?' + urlencode(basedict)
-        f = urlopen(fullurl)
-        ret = json.load(f)
-        f.close()
-        
-        memcache.incr("etsy_requests_%s" % dt.date.today(),
-                      initial_value = 0)        
-        return ret
-
-    def getUserDetails(self, user_id, **params):
-        path = '/users/'
-        a = {'user_id':user_id}
-        a.update(params)
-        r = self._make_call(path, a)
-        u = EtsyUser(self, r['results'][0])
-        return u
-
-    def getUsersByName(self, search_name, **params):
-        path = '/users/keywords/%s' % search_name
-        r = self._make_call(path, params)
-        return [EtsyUser(self, u) for u in r['results']]
-
-    def getShopDetails(self, user_id, **params):
-        path = '/shops/%s' % user_id
-        r = self._make_call(path, params)
-        return EtsyShop(self, r['results'][0])
-
-    def getFeaturedSellers(self, **params):
-        path = '/shops/featured'
-        r = self._make_call(path, params)
-        return [EtsyShop(self, u) for u in r['results']]
-
-    def getListings(self, user_id, **params):
-        path = '/shops/%s/listings' % user_id
-        r = self._make_call(path, params)
-        return [EtsyListing(self, u) for u in r['results']]
-
-    def getShopsByName(self, search_name, **params):
-        path = '/shops/keywords/%s' % search_name
-        r = self._make_call(path, params)
-        return [EtsyShop(self, u) for u in r['results']]
-
-    def getFeaturedDetails(self, user_id, **params):
-        path = '/shops/%s/listings/featured' % user_id
-        r = self._make_call(path, params)
-        return [EtsyListing(self, u) for u in r['results']]
-
-    def getListingDetails(self, listing_id, **params):
-        path = '/listings/%s' % listing_id
-        r = self._make_call(path, params)
-        return EtsyListing(self, r['results'][0])
-
-    def getListingsByTags(self, tags, **params):
-        path = '/listings/tags/%s' % tags
-        r = self._make_call(path, params)
-        return [EtsyListing(self, u) for u in r['results']]
-
-    def getListingsByKeyword(self, search_terms, **params):
-        path = '/listings/keywords/%s' % search_terms
-        r = self._make_call(path, params)
-        return [EtsyListing(self, u) for u in r['results']]
-
-    def getFrontFeaturedListings(self, **params):
-        path = '/listings/featured/front'
-        r = self._make_call(path, params)
-        return [EtsyListing(self, u) for u in r['results']]
-
-    def getTopTags(self):
-        path = '/tags/top'
-        r = self._make_call(path, {})
-        return r['results']
-
-    def getChildTags(self, tag):
-        path = 'tags/%s/children' % tag
-        r = self._make_call(path, {})
-        return r['results']
-
-    def getFavorersOfShop(self, user_id, **params):
-        path = '/shops/%s/favorers' % user_id
-        r = self._make_call(path, params)
-        return [EtsyUser(self, u) for u in r['results']]
-
-    def getFavoriteShopsOfUser(self, user_id, **params):
-        path = '/users/%s/favorites/shops' % user_id
-        r = self._make_call(path, params)
-        return [EtsyShop(self, u) for u in r['results']]
-
-    def getFavoriteListingsOfUser(self, user_id, **params):
-        path = '/users/%s/favorites/listings' % user_id
-        r = self._make_call(path, params)
-        return [EtsyListing(self, u) for u in r['results']]
-
-    def getFavorersOfListing(self, listing_id, **params):
-        path = '/listings/%s/favorers' % listing_id
-        r = self._make_call(path, params)
-        return [EtsyUser(self, u) for u in r['results']]
-
-    def getGiftGuides(self):
-        path = '/gift-guides'
-        r = self._make_call(path)
-        return [EtsyGiftGuide(self, u) for u in r['results']]
-
-    def getGiftGuideListings(self, guide_id, **params):
-        path = '/gift-guides/%s/listings' % guide_id
-        r = self._make_call(path, params)
-        return [EtsyGiftGuide(self, u) for u in r['results']]
-
-    def getMethodTable(self):
-        path = '/'
-        r = self._make_call(path, {})
-        return [EtsyMethod(self, u) for u in r['results']]
-
-    def ping(self):
-        path = '/server/ping'
-        r = self._make_call(path, {})
-        return r['results'][0]
-
-    def getServerEpoch(self):
-        path = '/server/epoch'
-        r = self._make_call(path, {})
-        return r['results'][0]
-
-def getAll(func, max_ret = None, **params):
-    max_pag = 50
-    cur_offset = 0
-    n_returned = 50
-    ret = []
-    while n_returned == max_pag and  (max_ret == None or  cur_offset < max_ret):
-        r = func(limit=max_pag, offset = cur_offset, **params)
-        n_returned = len(r)
-        cur_offset += max_pag
-        ret += r
-
-    return ret
-
-class EtsyResource (object):
-    def __init__(self, etsy, d):
+class Resource(object):
+    def __init__(self, d):
         self.__dict__ = d
-        self.etsy = etsy
 
     def __repr__(self):
         return '<' + ', '.join(['%s=%s' % (key, value.__repr__())
                                 for key, value
                                 in self.__dict__.iteritems()]) + '>'
 
-class EtsyGiftGuide(EtsyResource):
-    def getListings(self, **params):
-        return self.etsy.getGiftGuideListings(self.guide_id, **params)
+
+class Etsy2(object):
+    base_url = "http://openapi.etsy.com/v2/sandbox/public"
+
+    def __init__(self, api_key):
+        self.__api_key = api_key
 
 
-class EtsyListing(EtsyResource):
-    def getFavorers(self, **params):
-        return self.etsy.getFavorersOfListing(self.listing_id, **params)
+    def _make_call(self, path, params = {}):
+        basedict = {"api_key":self.__api_key}
+        basedict.update(params)
+        fullurl = self.base_url + path + '?' + urlencode(basedict)
+        logging.info(fullurl)
 
-class EtsyUser(EtsyResource):
-    def getFavoriteListings(self, **params):
-        return self.etsy.getFavoriteListingsOfUser(self.user_id, **params)
+        f = urlopen(fullurl)
+        ret = json.load(f)
+        f.close()
 
-    def getFavoriteShops(self, **params):
-        return self.etsy.getFavoriteShopsOfUser(self.user_id, **params)
+        memcache.incr("etsy_requests_%s" % dt.date.today(),
+                      initial_value = 0)
+        return ret
 
-    def getShopDetails(self, **params):
-        return self.etsy.getShopDetails(self.user_id, **params)
-
-class EtsyShop(EtsyUser):
-    def getFavorers(self, **params):
-        return self.etsy.getFavorersOfShop(self.user_id, **params)
-
-    def getListings(self, **params):
-        return self.etsy.getListings(self.user_id, **params)
-
-    def getFeaturedDetails(self, **params):
-        return self.etsy.getFeaturedDetails(self.user_id, **params)
-
-#Just used for method table:
-class EtsyMethod(EtsyResource):
-    pass
+    def ping(self):
+        path = '/server/ping'
+        r = self._make_call(path, {})
+        return r['results'][0]
 
 
+    def getResource(self, resource, id, includes = []):
+        path = '/%s/%s' % (resource, id)
+        params = {'includes': ",".join(includes)}
+        response = self._make_call(path, params)
+
+        res = []
+        for row in response['results']:
+            for include in includes:
+                if ":" in include:
+                    include = include.split(":", 1)[0]
+
+                if isinstance(row[include], list):
+                    row[include] = [Resource(item) for item in row[include]]
+                else:
+                    row[include] = Resource(row[include])
+            res.append(Resource(row))
+        return res
+
+
+    def getUser(self, user_name, includes = []):
+        return self.getResource("users", user_name, includes)[0]
+
+    def getShop(self, shop_name, includes = []):
+        return self.getResource("shops", shop_name, includes)[0]
+
+    def getListing(self, listing_id, includes = []):
+        return self.getResource("listings", listing_id, includes)[0]
